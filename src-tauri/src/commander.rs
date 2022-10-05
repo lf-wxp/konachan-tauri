3410a03fc50eff8c8a6f896660a46bdc6db6e848
franciscowxp <franciscowxp@gmail.com>
2022-10-05 20:13:54 +0800

    feat: use web page to controll splashcreen

--- src-tauri/src/commander.rs
+++ src-tauri/src/commander.rs
@@ -1,5 +1,5 @@
 use crate::image;
-use tauri::AppHandle;
+use tauri::{AppHandle, Manager};
 
 #[tauri::command]
 pub async fn get_post(page: i8, tags: String, mode: String) -> image::ApiResponse {
@@ -24,4 +24,14 @@ pub async fn download_image(app_handle: AppHandle, url: String) {
   if (image::download_image_progress_strut(url, &mut progress).await).is_err() {
     progress.error();
   };
+}
+
+#[tauri::command]
+pub async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // Show main window
+  window.get_window("main").unwrap().show().unwrap();
 }
