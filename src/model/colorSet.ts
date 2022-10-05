1fda50ba0a2addac8ac9b4e9e28f8f872f3cd3db
franciscowxp <franciscowxp@gmail.com>
2022-10-05 12:57:09 +0800

    feat: remove src directory

--- src/model/colorSet.ts
+++
@@ -1,4 +0,0 @@
-export interface ColorSet {
-  vibrant: string;
-  muted: string;
-}
--- src/model/imageDom.ts
+++
@@ -1,9 +0,0 @@
-import { ImageDetail } from './image';
-import { CSSProperties } from 'react';
-
-export interface ImageDom extends ImageDetail {
-  styleH?: number;
-  styleW?: number;
-  style?: CSSProperties;
-  full?: boolean;
-}
